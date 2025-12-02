7d7578f5589b84e8afa8e0d73927aaf574d31fb7
vanessalyzette14-dotcom <vanessalyzette14@gmail.com>
2025-12-02 15:48:24 -0700

    Part 1, first bullet done.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -39,7 +39,51 @@ console.log(isValid);
 // but it SHOULD NOT be done this way. As programmers,
 // we break things into small, manageable pieces so that
 // they can be better understood, scaled, and maintained.
-const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
-  ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
-  (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
+    ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) &&
+    !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
+    (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+// Math Problem Part 1
+
+
+// Check if all numbers are divisible by 5. Cache the result in a variable.
+const allDivisibleBy5 = (n1 % 5 === 0) && (n2 % 5 === 0) && (n3 % 5 === 0) && (n4 % 5 === 0);
+console.log(allDivisibleBy5);
+// Check if the first number is larger than the last. Cache the result in a variable
+
+//Accomplish the following arithmetic chain:
+//Subtract the first number from the second number.
+//Multiply the result by the third number.
+//Find the remainder of dividing the result by the fourth number.
+
+
+// Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+
+
+
+// Math Problem Part 2
+
+// Let’s look at a more practical scenario.
+// You are planning a cross-country road trip!
+
+
+// Your car’s fuel efficiency is as follows:
+// At 55 miles per hour, you get 30 miles per gallon.
+// At 60 miles per hour, you get 28 miles per gallon.
+// At 75 miles per hour, you get 23 miles per gallon.
+
+// You have a fuel budget of $175.
+
+// The average cost of fuel is $3 per gallon.
+
+// Set up a program to answer the following questions:
+// How many gallons of fuel will you need for the entire trip?
+
+// Will your budget be enough to cover the fuel expense?
+
+// How long will the trip take, in hours?
+
+// Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+// Log the results of your calculations using string concatenation or template literals.
