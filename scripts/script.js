a0531224b08c18b81a0dc2a1dd8fcc3262a592f5
vanessalyzette14-dotcom <vanessalyzette14@gmail.com>
2025-12-14 23:02:47 -0700

    reviewed

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,81 @@
+// // ------------------------------------------------------------------------------------------------
+// // The initial numbers that must be verified.
+// const n1 = 10;
+// const n2 = 15;
+// const n3 = 20;
+// const n4 = 5;
+
+// // Check one: add up to 50
+// // This is a fairly simple operation using
+// // arithmetic operators and a comparison.
+// const isSum50 = (n1 + n2 + n3 + n4) == 50;
+
+// // Check two: at least two odd numbers
+// // Here, we use modulus to check if something is odd.
+// // Since % 2 is 0 if even and 1 if odd, we can use
+// // arithmetic to count the total number of odd numbers.
+// const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+
+// // Check three: no number larger than 25
+// // This time, we use the OR operator to check
+// // if ANY of the numbers is larger than 25.
+// const isOver25 = n1 <= 25 || n2 <= 25 || n3 <= 25 || n4 <= 25;
+// console.log('IsOver25', isOver25)
+
+// // Check four: all unique numbers
+// // This is long, and there are more efficient
+// // ways of handling it with other data structures
+// // that we will review later.
+// const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
+
+// // Here, we put the results into a single variable 
+// // for convenience. Note how we negate isOver25 using
+// // the ! operator. We could also have tested for 
+// // "isUnder25" as an alternative.
+// const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
+
+// // Finally, log the results.
+// console.log(isValid);
+
+// // Here's another example of how this COULD be done,
+// // but it SHOULD NOT be done this way. As programmers,
+// // we break things into small, manageable pieces so that
+// // they can be better understood, scaled, and maintained.
+// const dontDoThis = ((n1 + n2 + n3 + n4) == 50) && 
+//   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
+//   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
+//   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+// ------------------------------------------------------------------------------------------------
+
 // The initial numbers that must be verified.
-const n1 = 10;
-const n2 = 15;
-const n3 = 20;
-const n4 = 5;
+// const n1 = 10;
+// const n2 = 15;
+// const n3 = 20;
+// const n4 = 5;
 
-// 1. Better defined the question
-// // CHeck the input/started code
-//  // reread, reread, reread, reread the question
-// 2.  Define or better unstand expected outputs
 
-//   arithmetic - math - number
-// Logic - and or NOT - return bool
-// comparison - return bool
+// // 1.   Check if all numbers are divisible by 5. Cache the result in a variable.
+// // Excpected output: variable containing a boolean
+// const isDivBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
+// console.log("isDiv5:", isDivBy5);
 
-// 1.   Check if all numbers are divisible by 5. Cache the result in a variable.
-// Excpected output: variable containing a boolean
-const isDivBy5 = (n1 % 5) + (n2 % 5) + (n3 % 5) + (n4 % 5) == 0;
-console.log("isDiv5:", isDivBy5);
+// // 2.   Check if the first number is larger than the last. Cache the result in a variable.
+// const isFirstLargerThanLast = n1 > n4;
+// console.log("isFirstLargerThanLast:", isFirstLargerThanLast);
 
-// 2.   Check if the first number is larger than the last. Cache the result in a variable.
-const isFirstLargerThanLast = n1 > n4;
-console.log("isFirstLargerThanLast:", isFirstLargerThanLast);
+// // 3.   Accomplish the following arithmetic chain:
+// // ✅ Subtract the first number from the second number.
+// // ✅ Multiply the result by the third number.
+// // ✅ Find the remainder of dividing the result by the fourth number.
+// let result = n2 - n1;
+// // result = result * n3;
+// result *= n3
+// result %= n4
+// console.log('Result:', result);
 
-// 3.   Accomplish the following arithmetic chain:
-// ✅ Subtract the first number from the second number.
-// ✅ Multiply the result by the third number.
-// ✅ Find the remainder of dividing the result by the fourth number.
-let result = n2 - n1;
-// result = result * n3;
-result *= n3
-result %= n4
-console.log('Result:', result);
+// // 4.   ✅ Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
 
-// 4.   ✅ Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
-
-// Check one: add up to 50
-// This is a fairly simple operation using
-// arithmetic operators and a comparison.
-const isSum50 = n1 + n2 + n3 + n4 == 50;
-
-// Check two: at least two odd numbers
-// Here, we use modulus to check if something is odd.
-// Since % 2 is 0 if even and 1 if odd, we can use
-// arithmetic to count the total number of odd numbers.
-const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-
-// Check three: no number larger than 25
-// This time, we use the OR operator to check
-// if ANY of the numbers is larger than 25.
-const isOver25 = n1 <= 25 || n2 <= 25 || n3 <= 25 || n4 <= 25;
-console.log('IsOver25:', isOver25)
-
-// Check four: all unique numbers
-// This is long, and there are more efficient
-// ways of handling it with other data structures
-// that we will review later.
-const isUnique =
-  n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-
-// Here, we put the results into a single variable
-// for convenience. Note how we negate isOver25 using
-// the ! operator. We could also have tested for
-// "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && isOver25 && isUnique;
-
-// Finally, log the results.
-console.log(isValid);
-
-// Here's another example of how this COULD be done,
-// but it SHOULD NOT be done this way. As programmers,
-// we break things into small, manageable pieces so that
-// they can be better understood, scaled, and maintained.
-const dontDoThis =
-  n1 + n2 + n3 + n4 == 50 &&
-  (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2 &&
-  !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) &&
-  n1 != n2 &&
-  n1 != n3 &&
-  n1 != n4 &&
-  n2 != n3 &&
-  n2 != n4 &&
-  n3 != n4;
 
 
 
@@ -115,8 +110,8 @@ const costPerGal = 3;
 const budget = 175;
 
 //  Dynamic Variables
-let speed = 75;
-let mpg = 23;
+let speed = 60;
+let mpg = 28;
 
 // Formula Variables
 let totalGallons = distance / mpg;
